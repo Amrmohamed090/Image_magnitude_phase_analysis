7f440e4e662b4842ebbf64ec643bda2249213ba0
Adham <81479927+Adham-Mohammed@users.noreply.github.com>
2022-12-23 22:39:34 +0200

    resize image and save it

--- combine.py
+++ combine.py
@@ -5,27 +5,18 @@ import numpy as np
 # import rgb2gray
 from skimage.color import rgb2gray
 
-img_1=cv2.imread(r"static/images/man.jpg")
-img_1=rgb2gray(img_1)
-img_2=cv2.imread(r"static/images/bird.jpg")
-img_2=rgb2gray(img_2)
+img_1=cv2.imread(r"static/images/man.jpg",cv2.IMREAD_GRAYSCALE)
+img_2=cv2.imread(r"static/images/bird.jpg",cv2.IMREAD_GRAYSCALE)
+
+#resize image and save it
 img_1=cv2.resize(img_1 ,(640 , 426))
 img_2=cv2.resize(img_2 ,(640 , 426))
-filename="static/images/man1.jpg"
+filename="static/images/man1.png"
+filename2="static/images/bird1.png"
 cv2.imwrite(filename, img_1)
-#resize image and change it to gray scale
-# plt.imshow(img_1 , cmap='gray')
-# plt.savefig("static/images/man1.jpg")
-# plt.imshow(img_2, cmap='gray')
-# plt.savefig("static/images/bird1.jpg")
-# plt.switch_backend('agg')
+cv2.imwrite(filename2, img_2)
 
 
-#plotting section
-# fig=plt.figure(figsize=(10,7))
-# fig.add_subplot(1,2,1)
-# fig.add_subplot(1,2,2)
-# plt.show()
 #Extraction of frequencies of first image
 fourier_1=np.fft.fft2((img_1)) #fft2 for 2d fourier transform as the variation of the image happend in two dimension 
 fourier_1_shifted = np.fft.fftshift(fourier_1) # to avoid the repeation in the frequencies
@@ -37,29 +28,9 @@ fourier_2_shifted = np.fft.fftshift(fourier_2)
 mag_2=np.abs(fourier_2_shifted) 
 phase_2=np.angle(fourier_2_shifted)
 
-# fig=plt.figure(figsize=(10,7))
-# fig.add_subplot(2,2,1)
-# plt.title("magnitude image 2")
-# plt.imshow(np.log(mag_2), cmap='gray')
-# fig.add_subplot(2,2,2)
-# plt.title("phase image 2")
-# plt.imshow(phase_2,cmap='gray')
-# fig.add_subplot(2,2,3)
-# plt.title("magnitude image 1")
-# plt.imshow(np.log(mag_1) , cmap='gray')
-# fig.add_subplot(2,2,4)
-# plt.title("phase image 1")
-# plt.imshow(phase_1,cmap='gray')
-# plt.show()
-
+#combine
 mag_1_phase_sphinx= np.multiply((mag_1), np.exp(1j*phase_2))
 mag_shpin_phase_1= np.multiply((mag_2), np.exp(1j*phase_1))
 
 img_combined=np.real(np.fft.ifft2(np.fft.ifftshift(mag_1_phase_sphinx)))
 img_combined_2=np.real(np.fft.ifft2(np.fft.ifftshift(mag_shpin_phase_1)))
-
-# fig=plt.figure(figsize=(10,7))
-# fig.add_subplot(2,2,1)
-# plt.imshow(img_combined , cmap='gray')    
-# fig.add_subplot(2,2,2)
-# plt.imshow(img_combined_2 , cmap='gray')
